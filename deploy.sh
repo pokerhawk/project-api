780069721a4712f31500949b40038135437ae370
pokerhawk <eliabedosreis@gmail.com>
2024-12-30 14:51:20 -0300

    add line

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-echo "ENV DATABASE_URL="postgres://DeliveryDB:DeliveryDB@2024@$(hostname -I | awk '{print $2}'):5432/DeliveryDB"" >> ./Dockerfile
+echo -e "\nENV DATABASE_URL="postgres://DeliveryDB:DeliveryDB@2024@$(hostname -I | awk '{print $2}'):5432/DeliveryDB"" >> ./Dockerfile
 
 docker compose -f ./db/docker-compose.yml up -d
 # docker compose -f ./docker-compose.yml down
