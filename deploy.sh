1dbf0ef6bb5498c4482643b3e73223dfb186952e
pokerhawk <eliabedosreis@gmail.com>
2024-12-31 17:41:22 -0300

    agr vai vey final

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,12 @@
 
 # echo -e "\nENV DATABASE_URL="postgres://DeliveryDB:DeliveryDB@2024@$(hostname -I | awk '{print $2}'):5432/DeliveryDB"" >> ./Dockerfile
 
+
 docker stop backend-app
 docker rm backend-app
 docker rmi delivery-backend-api
+
+echo y | docker system prune
 
 # docker compose -f ./db/docker-compose.yml up -d
 docker compose -f ./docker-compose.yml up -d
