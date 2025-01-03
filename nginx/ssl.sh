234690ed4efc34c4dba36ce5abc95bfb6ad2ce2a
pokerhawk <eliabedosreis@gmail.com>
2025-01-03 14:49:47 -0300

    troubleshoot

--- nginx/ssl.sh
+++ nginx/ssl.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-mkdir ../certs
+echo "creating certs dir"
+mkdir certs
+pwd
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout ../certs/nginx-selfsigned.key \
-    -out ../certs/nginx-selfsigned.crt
+    -keyout certs/nginx-selfsigned.key \
+    -out certs/nginx-selfsigned.crt
