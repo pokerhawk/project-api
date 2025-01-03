e001bab9adf291cd4b418a1b2b16f8623dfa700f
pokerhawk <eliabedosreis@gmail.com>
2025-01-03 14:56:05 -0300

    nginx certs

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,12 @@ docker rmi project-api-backend-app project-api-nginx-proxy
 
 echo y | docker system prune
 
-chmod +x ./nginx/ssl.sh
-./nginx/ssl.sh
-chmod -x ./nginx/ssl.sh
+mkdir certs
+
+## SSL cert ##
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout certs/nginx-selfsigned.key \
+    -out certs/nginx-selfsigned.crt
 
 if ! docker ps -a | grep -q "project-database"; then
     docker compose -f ./db/docker-compose.yml up -d
--- nginx/ssl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "creating certs dir"
-mkdir certs
-pwd
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout certs/nginx-selfsigned.key \
-    -out certs/nginx-selfsigned.crt
