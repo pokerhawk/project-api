2a6786c54c4525458a0576bd06c3b342d12ccf1d
pokerhawk <eliabedosreis@gmail.com>
2025-01-03 14:44:31 -0300

    cert location

--- nginx/ssl.sh
+++ nginx/ssl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir certs
+mkdir ../certs
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout certs/nginx-selfsigned.key \
-    -out certs/nginx-selfsigned.crt
+    -keyout ../certs/nginx-selfsigned.key \
+    -out ../certs/nginx-selfsigned.crt
