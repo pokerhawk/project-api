755729362641102010ec4118d5faa434577ff069
pokerhawk <eliabedosreis@gmail.com>
2025-01-06 17:17:04 -0300

    certbot

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,19 @@ echo y | docker system prune
 
 mkdir ~/project-api/nginx/certs
 
-## SSL cert ##
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout ~/project-api/nginx/certs/nginx-selfsigned.key \
-    -out ~/project-api/nginx/certs/nginx-selfsigned.crt \
-    -subj "/C=BR/ST=Minas Gerais/L=Belo Horizonte/O=My Company/OU=IT Department/CN=localhost"
+## SSL manual cert ##
+# openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+#     -keyout ~/project-api/nginx/certs/nginx-selfsigned.key \
+#     -out ~/project-api/nginx/certs/nginx-selfsigned.crt \
+#     -subj "/C=BR/ST=Minas Gerais/L=Belo Horizonte/O=My Company/OU=IT Department/CN=localhost"
 
 if ! docker ps -a | grep -q "project-database"; then
     docker compose -f ./db/docker-compose.yml up -d
 fi
 
 docker compose -f ./docker-compose.yml up -d
+
+unset DOMAIN_NAME
 
 #docker exec -it <container_name> /bin/bash
 #docker logs <container_name>
