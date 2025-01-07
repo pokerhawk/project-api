34c74fee75bf6689e68d9f01ba4d2f2accba542f
pokerhawk <eliabedosreis@gmail.com>
2025-01-06 22:26:29 -0300

    back to manual ssl

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,10 @@ mkdir ~/node_project
 mkdir ~/node_project/views
 
 ## SSL manual cert ##
-# openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-#     -keyout ~/project-api/nginx/certs/nginx-selfsigned.key \
-#     -out ~/project-api/nginx/certs/nginx-selfsigned.crt \
-#     -subj "/C=BR/ST=Minas Gerais/L=Belo Horizonte/O=My Company/OU=IT Department/CN=localhost"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout ~/project-api/nginx/certs/nginx-selfsigned.key \
+    -out ~/project-api/nginx/certs/nginx-selfsigned.crt \
+    -subj "/C=BR/ST=Minas Gerais/L=Belo Horizonte/O=My Company/OU=IT Department/CN=localhost"
 
 if ! docker ps -a | grep -q "project-database"; then
     docker compose -f ./db/docker-compose.yml up -d
